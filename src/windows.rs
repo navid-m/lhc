6529eb2f5a111585b9b71d9d0d74a9352fc840ba
Navid M <navidmomtahen@gmail.com>
2026-03-29 16:58:53 +0100

    inline

--- src/windows.rs
+++ src/windows.rs
@@ -1,10 +1,7 @@
 #[cfg(windows)]
 pub fn set_console_output_cp_utf8() -> Result<(), std::io::Error> {
     use windows_sys::Win32::System::Console::SetConsoleOutputCP;
-
-    const CP_UTF8: u32 = 65001;
-
-    let success = unsafe { SetConsoleOutputCP(CP_UTF8) };
+    let success = unsafe { SetConsoleOutputCP(65001) };
     if success == 0 {
         Err(std::io::Error::last_os_error())
     } else {
