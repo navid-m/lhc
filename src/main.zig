d203d42b8cfa1befb142558fcf6b2d3fb6b6a989
Navid M <navidmomtahen@gmail.com>
2026-03-29 15:11:38 +0100

    simplify

--- src/main.zig
+++ src/main.zig
@@ -13,10 +13,7 @@ pub fn main() !void {
         }
     }
 
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-
+    const allocator = std.heap.page_allocator;
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
